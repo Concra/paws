9df5eca20c2f98749fc14d5d8fee518e83aa0fab
Brad <Concra@users.noreply.github.com>
2025-02-02 15:38:14 -0500

    feat: Member and detail view.

--- paws_portal/members/views.py
+++ paws_portal/members/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from members.models import Member
 
 
 def members(request):
-    return HttpResponse('Members page')
+    paw_members = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'paw_members': paw_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    paw_member = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'paw_member': paw_member,
+    }
+    return HttpResponse(template.render(context, request))
