fb5198d4e061f01c29857bb4a3fa64284005389d
Brad <Concra@users.noreply.github.com>
2025-02-03 22:54:59 -0500

    Regen and moving secret key to env.

--- paws_portal/paws_portal/settings.py
+++ paws_portal/paws_portal/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mdr2&mpvx$@w1%z&l5ew%^s_*sa!yc543asqg)h0c5a=hit-k$'
+SECRET_KEY = env_config.get('ENV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
